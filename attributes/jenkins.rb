5a97f470f61a19aed4fe1bb6ce3d4820cf04d7d6
Erickson Delgado <ericksond@gmail.com>
2016-08-10 23:53:15 -0400

    Defaulted to Jenkins 2.17-11

--- attributes/jenkins.rb
+++ attributes/jenkins.rb
@@ -1,3 +1,3 @@
-default['jenkins']['master']['version'] = '2.9-1.1' # LTS
+default['jenkins']['master']['version'] = '2.17-1.1' # LTS
 default['jenkins']['master']['jvm_options'] = '-Xms256m -Xmx256m'
 default['jenkins']['master']['listen_address'] = '127.0.0.1'
